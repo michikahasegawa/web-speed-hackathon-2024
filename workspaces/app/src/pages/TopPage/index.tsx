2489f779b742a7fab6a1c33b993088664ea34f10
mhasegawa <153479651+michikahasegawa@users.noreply.github.com>
2024-03-24 10:48:40 +0900

    Update index.tsx

--- workspaces/app/src/pages/TopPage/index.tsx
+++ workspaces/app/src/pages/TopPage/index.tsx
@@ -1,6 +1,4 @@
-import _ from 'lodash';
-import moment from 'moment-timezone';
-import { Suspense, useId } from 'react';
+import { Suspense, useMemo } from 'react';
 
 import { BookCard } from '../../features/book/components/BookCard';
 import { FeatureCard } from '../../features/feature/components/FeatureCard';
@@ -18,14 +16,12 @@ import { getDayOfWeekStr } from '../../lib/date/getDayOfWeekStr';
 import { CoverSection } from './internal/CoverSection';
 
 const TopPage: React.FC = () => {
-  const todayStr = getDayOfWeekStr(moment());
+  const todayStr = useMemo(() => getDayOfWeekStr(moment()), []); // 日付をキャッシュする
+
+  // データをフェッチ
   const { data: release } = useRelease({ params: { dayOfWeek: todayStr } });
   const { data: featureList } = useFeatureList({ query: {} });
   const { data: rankingList } = useRankingList({ query: {} });
-
-  const pickupA11yId = useId();
-  const rankingA11yId = useId();
-  const todayA11yId = useId();
 
   return (
     <Flex align="flex-start" direction="column" gap={Space * 2} justify="center" pb={Space * 2}>
@@ -33,14 +29,14 @@ const TopPage: React.FC = () => {
         <CoverSection />
       </Box>
       <Box as="main" maxWidth="100%" width="100%">
-        <Box aria-labelledby={pickupA11yId} as="section" maxWidth="100%" mt={16} width="100%">
-          <Text as="h2" color={Color.MONO_100} id={pickupA11yId} typography={Typography.NORMAL20} weight="bold">
+        <Box aria-labelledby="pickupA11yId" as="section" maxWidth="100%" mt={16} width="100%">
+          <Text as="h2" color={Color.MONO_100} id="pickupA11yId" typography={Typography.NORMAL20} weight="bold">
             ピックアップ
           </Text>
           <Spacer height={Space * 2} />
           <Box maxWidth="100%" overflowX="scroll" overflowY="hidden">
             <Flex align="stretch" direction="row" gap={Space * 2} justify="flex-start">
-              {_.map(featureList, (feature) => (
+              {featureList.map((feature) => (
                 <FeatureCard key={feature.id} bookId={feature.book.id} />
               ))}
             </Flex>
@@ -49,14 +45,14 @@ const TopPage: React.FC = () => {
 
         <Spacer height={Space * 2} />
 
-        <Box aria-labelledby={rankingA11yId} as="section" maxWidth="100%" width="100%">
-          <Text as="h2" color={Color.MONO_100} id={rankingA11yId} typography={Typography.NORMAL20} weight="bold">
+        <Box aria-labelledby="rankingA11yId" as="section" maxWidth="100%" width="100%">
+          <Text as="h2" color={Color.MONO_100} id="rankingA11yId" typography={Typography.NORMAL20} weight="bold">
             ランキング
           </Text>
           <Spacer height={Space * 2} />
           <Box maxWidth="100%" overflowX="hidden" overflowY="hidden">
             <Flex align="center" as="ul" direction="column" justify="center">
-              {_.map(rankingList, (ranking) => (
+              {rankingList.map((ranking) => (
                 <RankingCard key={ranking.id} bookId={ranking.book.id} />
               ))}
             </Flex>
@@ -65,14 +61,14 @@ const TopPage: React.FC = () => {
 
         <Spacer height={Space * 2} />
 
-        <Box aria-labelledby={todayA11yId} as="section" maxWidth="100%" width="100%">
-          <Text as="h2" color={Color.MONO_100} id={todayA11yId} typography={Typography.NORMAL20} weight="bold">
+        <Box aria-labelledby="todayA11yId" as="section" maxWidth="100%" width="100%">
+          <Text as="h2" color={Color.MONO_100} id="todayA11yId" typography={Typography.NORMAL20} weight="bold">
             本日更新
           </Text>
           <Spacer height={Space * 2} />
           <Box maxWidth="100%" overflowX="scroll" overflowY="hidden">
             <Flex align="stretch" gap={Space * 2} justify="flex-start">
-              {_.map(release.books, (book) => (
+              {release.books.map((book) => (
                 <BookCard key={book.id} bookId={book.id} />
               ))}
             </Flex>
