080fa13d7892660096b7097884e9d03deff85116
mhasegawa <153479651+michikahasegawa@users.noreply.github.com>
2024-03-24 12:44:49 +0900

    Update ComicViewerPage.tsx

--- workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
+++ workspaces/app/src/features/viewer/components/ComicViewerPage.tsx
@@ -1,13 +1,11 @@
-import { useRef } from 'react';
-import { useAsync } from 'react-use';
+import { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { decrypt } from '@wsh-2024/image-encrypt/src/decrypt';
 
 import { getImageUrl } from '../../../lib/image/getImageUrl';
 
-const _Canvas = styled.canvas`
-  height: auto; /* 高さを自動調整 */
+const Canvas = styled.canvas`
   width: 100%; /* 幅を100%に */
 `;
 
@@ -16,32 +14,37 @@ type Props = {
 };
 
 export const ComicViewerPage = ({ pageImageId }: Props) => {
-  const ref = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useAsync(async () => {
-    const image = new Image();
-    image.src = getImageUrl({
-      format: 'jxl',
-      imageId: pageImageId,
-    });
-    await image.decode();
+  useEffect(() => {
+    const loadPageImage = async () => {
+      const image = new Image();
+      image.src = getImageUrl({
+        format: 'jxl',
+        imageId: pageImageId,
+      });
+      await image.decode();
 
-    const canvas = ref.current!;
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    const ctx = canvas.getContext('2d')!;
+      const canvas = canvasRef.current!;
+      const ctx = canvas.getContext('2d')!;
+      
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
 
-    decrypt({
-      exportCanvasContext: ctx,
-      sourceImage: image,
-      sourceImageInfo: {
-        height: image.naturalHeight,
-        width: image.naturalWidth,
-      },
-    });
+      decrypt({
+        exportCanvasContext: ctx,
+        sourceImage: image,
+        sourceImageInfo: {
+          height: image.naturalHeight,
+          width: image.naturalWidth,
+        },
+      });
 
-    canvas.setAttribute('role', 'img');
+      canvas.setAttribute('role', 'img');
+    };
+
+    loadPageImage();
   }, [pageImageId]);
 
-  return <_Canvas ref={ref} />;
+  return <Canvas ref={canvasRef} />;
 };
