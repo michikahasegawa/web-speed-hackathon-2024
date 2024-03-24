9c416874c06ff7220648a946142a13f68d6892c3
mhasegawa <153479651+michikahasegawa@users.noreply.github.com>
2024-03-24 12:38:42 +0900

    Update ComicViewerCore.tsx

--- workspaces/app/src/features/viewer/components/ComicViewerCore.tsx
+++ workspaces/app/src/features/viewer/components/ComicViewerCore.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { addUnitIfNeeded } from '../../../lib/css/addUnitIfNeeded';
@@ -42,29 +42,72 @@ type Props = {
 
 const ComicViewerCore: React.FC<Props> = ({ episodeId }) => {
   const { data: episode } = useEpisode({ params: { episodeId } });
+  const [scrollView, scrollViewRef] = useState<HTMLDivElement | null>(null);
 
-  const [containerWidth, setContainerWidth] = useState(0);
-  const [pageCountParView, setPageCountParView] = useState(1);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const pageCountParViewRef = useRef<number>(1);
+  const pageWidthRef = useRef<number>(0);
+  const viewerPaddingInlineRef = useRef<number>(0);
 
   useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
     const handleResize = () => {
-      const container = document.getElementById('comic-container');
-      if (container) {
-        const cWidth = container.getBoundingClientRect().width;
-        setContainerWidth(cWidth);
-        setPageCountParView((cWidth / 100) * (IMAGE_HEIGHT / IMAGE_WIDTH) < 2 ? 1 : 2);
-      }
+      const cqw = container.getBoundingClientRect().width / 100;
+      const cqh = container.getBoundingClientRect().height / 100;
+
+      pageCountParViewRef.current = (100 * cqw) / (100 * cqh) < (2 * IMAGE_WIDTH) / IMAGE_HEIGHT ? 1 : 2;
+      pageWidthRef.current = ((100 * cqh) / IMAGE_HEIGHT) * IMAGE_WIDTH;
+      viewerPaddingInlineRef.current =
+        (100 * cqw - pageWidthRef.current * pageCountParViewRef.current) / 2 + (pageCountParViewRef.current === 2 ? pageWidthRef.current : 0);
     };
 
     handleResize();
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    const resizeObserver = new ResizeObserver(handleResize);
+    resizeObserver.observe(container);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const scrollView = scrollViewRef.current;
+    if (!scrollView) return;
+
+    const handleScroll = () => {
+      const scrollViewCenterX = scrollView.getBoundingClientRect().width / 2;
+      const children = Array.from(scrollView.children) as HTMLDivElement[];
+
+      let scrollToLeft = Number.MAX_SAFE_INTEGER;
+
+      for (const child of children) {
+        const childCenterX = child.getBoundingClientRect().left + child.getBoundingClientRect().width / 2;
+        const candidateScrollToLeft = childCenterX - scrollViewCenterX;
+
+        if (Math.abs(candidateScrollToLeft) < Math.abs(scrollToLeft)) {
+          scrollToLeft = candidateScrollToLeft;
+        }
+      }
+
+      scrollView.scrollTo({
+        left: scrollView.scrollLeft + scrollToLeft,
+        behavior: 'smooth',
+      });
+    };
+
+    scrollView.addEventListener('scroll', handleScroll);
+
+    return () => {
+      scrollView.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
-    <Container id="comic-container">
-      <Wrapper $paddingInline={0} $pageWidth={IMAGE_WIDTH}>
+    <Container ref={containerRef}>
+      <Wrapper ref={scrollViewRef} $paddingInline={viewerPaddingInlineRef.current} $pageWidth={pageWidthRef.current}>
         {episode?.pages.map((page) => (
           <ComicViewerPage key={page.id} pageImageId={page.image.id} />
         ))}
@@ -73,4 +116,12 @@ const ComicViewerCore: React.FC<Props> = ({ episodeId }) => {
   );
 };
 
-export default ComicViewerCore;
+const ComicViewerCoreWithSuspense: React.FC<Props> = ({ episodeId }) => {
+  return (
+    <Suspense fallback={null}>
+      <ComicViewerCore episodeId={episodeId} />
+    </Suspense>
+  );
+};
+
+export { ComicViewerCoreWithSuspense as ComicViewerCore };
