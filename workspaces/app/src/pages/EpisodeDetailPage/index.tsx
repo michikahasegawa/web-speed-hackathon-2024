4f514fa536fc868e52db12afed26c9c38284e726
mhasegawa <153479651+michikahasegawa@users.noreply.github.com>
2024-03-24 10:15:33 +0900

    Update index.tsx

--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
-import type { RouteParams } from 'regexparam';
-import invariant from 'tiny-invariant';
+import { invariant } from 'tiny-invariant';
 
 import { useBook } from '../../features/book/hooks/useBook';
 import { EpisodeListItem } from '../../features/episode/components/EpisodeListItem';
@@ -23,15 +22,17 @@ const EpisodeDetailPage: React.FC = () => {
 
   return (
     <Box>
-      <section aria-label="漫画ビューアー">
-        <ComicViewer episodeId={episode.id} />
-      </section>
+      {episode && (
+        <section aria-label="漫画ビューアー">
+          <ComicViewer episodeId={episode.id} />
+        </section>
+      )}
 
       <Separator />
 
       <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
         <Flex align="center" as="ul" direction="column" justify="center">
-          {book.episodes.map((episode) => (
+          {book && book.episodes.map((episode) => (
             <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
           ))}
         </Flex>
